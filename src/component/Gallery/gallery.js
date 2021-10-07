e675bea297d5a5730aec70742b3606bf71d90641
Aswin Kumar <aswin.kumar.17@mountblue.tech>
2021-10-07 10:32:27 +0530

    Optimisation in componentDidMount

--- src/component/Gallery/gallery.js
+++ src/component/Gallery/gallery.js
@@ -18,25 +18,12 @@ class gallery extends Component {
     }
 
     componentDidMount() {
-        this.setState({ imgName: this.props.searchImage });
-
-        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=874723272ab0b8c11a40fc8c0769153f&tags=${this.props.searchImage}&per_page=24&page=1&format=json&nojsoncallback=1`)
-            .then(function (response) {
-                // console.log(response.data)
-                return response.data;
-            })
-            .then(function (data) {
-                let imageURL = data.photos.photo.map((pic) => {
-                    return 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg';
-                })
-                this.setState({ imageURL });
-            }.bind(this))
-            .catch(err => {
-                console.log("Not Found URL")
-            })
+        this.setState({ imgName: "" });
+        console.log("Mount")
     }
 
     componentDidUpdate(prevProps, prevState) {
+        console.log("Update")
         if (prevState.imgName !== this.props.searchImage) {
             this.setState({ imageFound: "" })
             this.setState({ imgName: this.props.searchImage })
